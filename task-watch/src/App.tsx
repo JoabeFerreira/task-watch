d0e792f61319ddd3629813be2cc31a835f0497b8
joabe-fazsoft <joabe@fazsoft.com.br>
2023-03-14 13:10:50 -0300

    feat: calculating time progress

--- task-watch/src/App.tsx
+++ task-watch/src/App.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
   const [progress, setProgress] = useState(0);
+  const timerRef = useRef<HTMLDivElement>(null)
+let intervalId;
+
+  const calculateProgress = () => {
+    intervalId = window.setInterval(() => {
+      if (!timerRef.current) return;
+      const newProgress = progress + 5
+      timerRef.current.style.background = `conic-gradient(#AA77FF ${newProgress}deg, #1a1a1a 0deg)`
+      setProgress(newProgress)
+    }, 1000)
+  }
+
   return (
     <div className="App">
-      <div className="timer">
+      <div ref={timerRef} className="timer">
         <div className="progress-display">
           {progress}%
         </div>
       </div>
+      <button onClick={() => calculateProgress()}>Start</button>
     </div>
   )
 }
