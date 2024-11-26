d42f20eef528c9ea2d0d86514b864498f3226a59
Joabe Ferreira Evangelista <joa3.bys@hotmail.com>
2024-11-26 20:40:09 -0300

    tests: removing time.test.ts

--- task-watch/src/tests/time.test.ts
+++
@@ -1,7 +0,0 @@
-import {Time} from '../models/time'
-
-test('should add 5 seconds', () => {
-  const time = new Time()
-  time.addSeconds(5) 
-  expect(time).toEqual({seconds: 5, minutes: 0, hours: 0})
-})
